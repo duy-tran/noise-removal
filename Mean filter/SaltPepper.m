8b858f39d4b6c82438c0170406567b6fc84dd41c
Duy <grimshady.z@gmail.com>
2016-12-18 15:17:43 +0700

    Add images + codes

---
+++ Mean filter/SaltPepper.m
@@ -0,0 +1,8 @@
+N3 = 1/9*ones(3,3);
+N5 = 1/25*ones(5,5);
+N7 = 1/49*ones(7,7);
+img_source = imread('../images/salt_pepper.png');
+img = im2double(img_source);
+filtered = filter2(N3,img);
+subplot(1,2,1), imshow(img)
+subplot(1,2,2), imshow(filtered)
